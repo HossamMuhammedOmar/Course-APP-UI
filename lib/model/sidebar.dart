d333a4e8f8d39d3a2d023f6f3175930a6a7c9a6e
Your Name <hossamomar9876@gmail.com>
2020-12-12 17:01:07 +0200

    finsih sidebar model

--- lib/model/sidebar.dart
+++ lib/model/sidebar.dart
@@ -6,3 +6,65 @@ class Sidebar {
   LinearGradient background;
   Icon icon;
 }
+
+var sidebarItem = [
+  Sidebar(
+    title: 'Home',
+    background: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [
+          Color(0XFF686ee2),
+          Color(0XFF382f60),
+        ]),
+    icon: Icon(
+      Icons.home,
+      color: Colors.white,
+    ),
+  ),
+  Sidebar(
+    title: 'Courses',
+    background: LinearGradient(
+      begin: Alignment.topRight,
+      end: Alignment.bottomLeft,
+      colors: [
+        Color(0xFFF7923A),
+        Color(0xffd35400),
+      ],
+    ),
+    icon: Icon(
+      Icons.library_books,
+      color: Colors.white,
+    ),
+  ),
+  Sidebar(
+    title: 'Subscribe',
+    background: LinearGradient(
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+      colors: [
+        Color(0xFFFA6C5C),
+        Color(0xffc0392b),
+      ],
+    ),
+    icon: Icon(
+      Icons.credit_card,
+      color: Colors.white,
+    ),
+  ),
+  Sidebar(
+    title: 'Settings',
+    background: LinearGradient(
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+      colors: [
+        Color(0xFF32E97E),
+        Color(0xff27ae60),
+      ],
+    ),
+    icon: Icon(
+      Icons.settings,
+      color: Colors.white,
+    ),
+  ),
+];
